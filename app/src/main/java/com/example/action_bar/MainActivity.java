a82e539b5284caf8405f2734dfd045539e2920e3
Jessika Suniga <jessikasuniga@gmail.com>
2021-10-30 15:13:11 -0300

    Action_Bar menu retray

---
+++ app/src/main/java/com/example/action_bar/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.action_bar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setOnClickListener(v -> actionBar.show());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_close){
+            actionBar.hide();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
